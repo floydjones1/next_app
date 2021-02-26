1da61cc5ec56eeab413d89af62fdc5df9af5393a
Floyd Jones <floyd-jones1@hotmail.com>
2021-02-25 19:23:03 -0500

    updated config since I know url

--- config/index.ts
+++ config/index.ts
@@ -1,3 +1,3 @@
 const dev = process.env.NODE_ENV !== 'production';
 
-export const baseURL = dev ? 'http://localhost:3000' : 'https://your_deployment.server.com';
+export const baseURL = dev ? 'http://localhost:3000' : 'https://next-app1-navy.vercel.app';
